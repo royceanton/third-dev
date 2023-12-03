2a76bd12fc380e88a16b47ef6b62a49826d7b2eb
Royce Anton Jose <106367095+royceanton@users.noreply.github.com>
2023-12-03 19:16:49 +0000

    third run

--- third-run-app/src/App.js
+++ third-run-app/src/App.js
@@ -5,12 +5,29 @@ import {
   embeddedWallet,
 } from '@thirdweb-dev/react';
 
+// Your custom UI components for the wallet connection
+//import DurchNetSelectUI from './DurchNetSelectUI'; // make sure to create this component
+//import DurchNetConnectUI from './DurchNetConnectUI'; // make sure to create this component
+
 function App() {
+  // Configuration for the embedded wallet with custom branding and UI
+  const embeddedWalletConfig = embeddedWallet({
+    auth: {
+      options: ["email", "facebook", "apple", "google"], // Customize as needed
+    },
+    meta: {
+      name: "Durch.net Wallet", // Your brand name for the wallet
+      //iconURL: "/path/to/your/brand/icon.png", // Path to your brand's icon
+    },
+    //connectUI: DurchNetConnectUI, // Your custom React component for connection UI
+    //selectUI: DurchNetSelectUI, // Your custom React component for selection UI
+  });
+
   return (
     <ThirdwebProvider
-      activeChain="goerli"
-      clientId= {process.env.REACT_APP_CLIENT_ID}
-      supportedWallets={[embeddedWallet()]}
+      activeChain="mainnet" // This sets the default chain to Ethereum Mainnet
+      clientId={process.env.REACT_APP_CLIENT_ID} // Client ID from your .env file
+      supportedWallets={[embeddedWalletConfig]} // Passing the customized config
     >
       <ConnectWallet />
     </ThirdwebProvider>
