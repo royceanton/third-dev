37af9aae921cb7606d4340df06a208cc6f0dc222
Royce Anton Jose <106367095+royceanton@users.noreply.github.com>
2023-12-03 18:49:40 +0000

    third run

--- third-run-app/src/App.js
+++ third-run-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  ThirdwebProvider,
+  ConnectWallet,
+  embeddedWallet,
+} from '@thirdweb-dev/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThirdwebProvider
+      activeChain="goerli"
+      clientId="bb92503f0218ec501594a4cd84d24cb0"
+      supportedWallets={[embeddedWallet()]}
+    >
+      <ConnectWallet />
+    </ThirdwebProvider>
   );
 }
 
